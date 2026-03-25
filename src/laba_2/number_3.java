b3a784e7834e956aacb7456220ba3a11a03d8fb0
whunotexplain <kozinegor2906@gmail.com>
2026-03-25 11:27:13 +0300

    full working laba 2

--- src/laba_2/number_3.java
+++ src/laba_2/number_3.java
@@ -1,4 +1,31 @@
 package laba_2;
 
-public class number_3 {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class number_3 extends Frame {
+    number_3(String str) {
+        super(str);
+        setSize(600, 500);
+        setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent v) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public void paint(Graphics g){
+        Color initColor = g.getColor();
+        g.setColor(new Color(0,0,255));
+        g.fillRect(0,0,getSize().width - 1, getSize().height - 1);
+        g.setColor(initColor);
+    }
+
+    public static void main(String[] args) {
+        Frame fr = new number_3("My first window");
+        fr.setSize(400, 400);
+        fr.setVisible(true);
+    }
 }
